8a6c82f183207b8046722806e6db362137c06d6c
Ramon Madeira Tomaz <ramonmadeira5@gmail.com>
2025-07-25 16:48:58 -0300

    Update removerTarefa.js

--- removerTarefa.js
+++ removerTarefa.js
@@ -1,17 +1,7 @@
 const BotaoRemover = () => {
-    const botaoRemover = document.createElement('button')
-
-    botaoRemover.classList.add('remover')
-
-    botaoRemover.innerText = '❌'
-
-
-    botaoRemover.addEventListener('click', () => {
-        console.log('teste');
-        
-    })
-
-    return botaoRemover
-}
-
-export default BotaoRemover
+  const botaoRemover = document.createElement("button");
+  botaoRemover.classList.add("remover");
+  botaoRemover.innerText = "❌";
+  return botaoRemover;
+};
+export default BotaoRemover;
