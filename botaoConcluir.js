fea648dc159f0757c073dbdaf4e846d18f4c96d9
Ramon Madeira Tomaz <ramonmadeira5@gmail.com>
2025-07-25 16:48:46 -0300

    Update and rename concluirTarefa.js to botaoConcluir.js

---
+++ botaoConcluir.js
@@ -0,0 +1,8 @@
+const BotaoConclui = () => {
+  const botaoConclui = document.createElement("button");
+  botaoConclui.classList.add("concluido");
+  botaoConclui.innerText = "✅";
+  return botaoConclui;
+};
+
+export default BotaoConclui;
